38a201e871f83c5961e61685664dec967376adf6
RemiMartin <mrmi11@yahoo.fr>
2014-06-02 19:59:36 +0200

    Beginning of rendering classes

---
+++ src/client/display/RenderManager.js
@@ -0,0 +1,33 @@
+/**
+ * 	@author       Rémi MARTIN <martinremi60@gmail.com>
+ * 	@copyright    2014 Rémi MARTIN
+ */
+
+ /**
+ * 	Creates a new RenderManager. This class is used to render the World.
+ * 	@class RenderManager
+ * 	@classdesc yom - RenderManager
+ * 	@constructor
+ * 	@return {yom.RenderManager} The renderManager object
+ */
+yom.RenderManager = function () {
+	/* NOTHING */
+}
+
+// ----- Method(s) ----- \\
+/**
+ * 	Render the world how it is when the function is called
+ * 	@method yom.Body#contains
+  *	@param {yom.World} world - The world we want to display.
+ * 	@return {boolean} True if the body contains the point, else false
+ */
+yom.RenderManager.prototype.display = function(world) {
+	var savedWorld = world;
+
+	var i;
+	for(i = 0; i < saveWorld.length; ++i) {
+		// RENDER
+		// TODO : define getPoints()
+		//saveWorld[i].getPoints();
+	}
+};
