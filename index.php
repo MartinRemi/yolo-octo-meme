4cc0672b296e55a6d0be1c5e2a069a78d336975b
RemiMartin <mrmi11@yahoo.fr>
2014-06-27 21:38:42 +0200

    Clean

--- index.php
+++ index.php
@@ -3,38 +3,6 @@
 	</head>
 
 	<body>
-		<!--
-		<?php
-		// DATABASE connection
-		// include "src/server/functions/databaseConnection.php";
-		// if($conn==null) { echo 'lol'; }
-		
-		// include 'src/server/model/Unit.php';
-		// include 'src/server/action/ServerAction.php';
-		// include 'src/server/action/TestAction.php';
-		// include 'src/server/server/ServerActionHandler.php';
-		// include 'src/server/server/EventHandler.php';
-		// include 'src/server/server/EventHandlerFactory.php';
-		// $unit = new Unit(1, 1, 1);
-		// $unit->persist();
-		// $gb = new GameBoard(1, 3, 3);
-		// echo $unit->getId()."<br />";
-		// echo $unit->getCoordinates()->getX();
-		// echo " | ";
-		// echo $unit->getCoordinates()->getY()."<br />";
-		
-		// echo $gb->getId()." ".$gb->getWidth()." ".$gb->getHeight()."<br />";
-		
-		// $action = ServerActionHandler::getInstance(new TestAction());
-		// $unit = Unit::getUnitById(3);
-		// echo $unit->getId().' | '.$unit->getCoordinates()->getX().' | '.$unit->getCoordinates()->getY();
-		
-		// $units = Unit::listUnits();
-		// foreach($units as $unit) {
-		// 	echo $unit->getId().'<br />';
-		// }
-		?>
-		-->
 		<canvas id="app" width="500" height="500">
 		</canvas>
 		
